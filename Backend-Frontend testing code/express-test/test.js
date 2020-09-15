47d84d33540345857ee30386bebf0e8886a525db
Mart-Jan Koedam <0962472@hr.nl>
2020-09-15 10:09:01 +0200

    got a working example to send html data to local server

--- Backend-Frontend testing code/express-test/test.js
+++ Backend-Frontend testing code/express-test/test.js
@@ -1,7 +1,28 @@
-const express = require('express') 
-const app = express() 
-const port = 3000 
- 
-app.get('/api', (req, res) => res.send('Hello World2!')) 
- 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+let cloudLib = require("crownstone-cloud")
+let cloud = new cloudLib.CrownstoneCloud();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port${port}`);
+});
+
+    let sendUserData;
+app.post('/test', (req, res) => {
+    async function login() {
+        console.log("Logging in...");
+        await cloud.login(req.body.name, req.body.password)
+        let myUserData = await cloud.me();
+        let userId       = await myUserData.id();
+        console.log(myUserData);
+        sendUserData = userId;
+    }
+    login().catch((e) => { console.log("There was a problem running this example:", e); });
+    console.log(sendUserData);
+    res.send('UserId: ' + sendUserData);
+})
