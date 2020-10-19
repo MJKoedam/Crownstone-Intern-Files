c081711d734a3367354dda400d81291d258dc865
MJKoedam <martjankoedam42@gmail.com>
2020-10-19 19:19:47 +0200

    updated planning

--- Testing code/userLocationTest.js
+++ Testing code/userLocationTest.js
@@ -2,44 +2,25 @@ let cloudLib = require("crownstone-cloud")
 let cloud = new cloudLib.CrownstoneCloud();
 
 //credentials:
-const crownstoneEmailAddress = "username";
-const crownstonePassword     = "password";
+const crownstoneEmailAddress = "martjankoedam42@gmail.com";
+const crownstonePassword     = "CrownstoneIntern";
 
-//function to login to the cloud and return the userdata
+let userReference;
+
 async function login() {
     await cloud.login(crownstoneEmailAddress, crownstonePassword)
 
-    let userReference = await cloud.me();
+    userReference = await cloud.me();
     console.log(userReference);
-
-    let userId       = await userReference.id();
-    console.log(userId);
-    let userData     = await userReference.data();
-    console.log(userData);
-    let userLocation = await userReference.currentLocation();
-    console.log(userLocation);
-    console.log(userLocation.length);
-    console.log("locatie: ");
-    console.log(userLocation[0]);
-
-    //GET SPHERE ID OF THE CURRENT LOCATION OF THE USER
-    let sphereId = userLocation[0]['inSpheres'][0]['sphereId'];
-    console.log(sphereId);
-
-    let allCs = await cloud.sphere(sphereId).crownstones();
-    for (let i = 0; i < allCs.length; i++){
-        console.log("This is Crownstone " + i + " with ID: " + allCs[i].id + " and name: " + allCs[i].name);
-    }
-    console.log("Extra:");
-    console.log(allCs[0]);
-
-    // let myUserData = await cloud.me().data()
-    // console.log(myUserData);
-    //
-    // let myLocationData = await cloud.me().currentLocation()
-    // console.log(myLocationData);
-    //
-    // let location = await cloud.locations()
-    // console.log(location);
 }
 login().catch((e) => { console.log("There was a problem running this code:", e); });
+
+async function myLoop() {
+    setTimeout(async function() {
+        let userLocation = await userReference.currentLocation();
+        console.log(userLocation[0]['inSpheres'][0]);
+        myLoop().catch((e) => { console.log("Something went wrong with looping!", e); });
+    }, 3000)
+}
+
+myLoop().catch((e) => { console.log("Something went wrong with looping!", e); });
