0f27b626de19be0e34c065a302417d47489817d9
MJKoedam <martjankoedam42@gmail.com>
2020-10-28 16:59:32 +0100

    changed code to test how different circumstances affect the information obtained by the cloud

--- Testing code/userLocationTest.js
+++ Testing code/userLocationTest.js
@@ -12,15 +12,20 @@ async function login() {
 
     userReference = await cloud.me();
     console.log(userReference);
+    let userLocation = await userReference.currentLocation();
+    console.log('userlocation: ' + userLocation.length);
+    console.log(userLocation);
+    console.log(userLocation[0]['inSpheres'][0]);
 }
 login().catch((e) => { console.log("There was a problem running this code:", e); });
 
 async function myLoop() {
     setTimeout(async function() {
         let userLocation = await userReference.currentLocation();
+        console.log('userlocation: ' + userLocation.length);
         console.log(userLocation[0]['inSpheres'][0]);
         myLoop().catch((e) => { console.log("Something went wrong with looping!", e); });
     }, 3000)
 }
 
-myLoop().catch((e) => { console.log("Something went wrong with looping!", e); });
+//myLoop().catch((e) => { console.log("Something went wrong with looping!", e); });
