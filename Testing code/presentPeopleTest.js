757475a4d3f2ba7f61a889c4ddb7221a3d7f12e9
MJKoedam <martjankoedam42@gmail.com>
2020-11-17 00:31:22 +0100

    changed code to test the presentPeople function

--- Testing code/presentPeopleTest.js
+++ Testing code/presentPeopleTest.js
@@ -16,5 +16,44 @@ async function login() {
 
     let presentPeople = await sphere.presentPeople();
     console.log(presentPeople);
+
+    // for (let i = 0; i < presentPeople.length; i++) {
+    //     console.log(presentPeople.every(testfunction));
+    // }
+
+    console.log(checkIfEqual(presentPeople, "5f4cbb44bfeb1e0004229a8c"));
+
 }
 login().catch((e) => { console.log("There was a problem running this code:", e); });
+
+function testfunction(userId) {
+    console.log('het userid:');
+    console.log(userId.userId);
+    return true;
+}
+
+function checkIfEqual(array, checkValue) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i].userId);
+        console.log(checkValue);
+        if (array[i].userId === checkValue) {
+            console.log('testing.. with checkvalue: ' + checkValue);
+            console.log(array);
+            console.log('splicing..');
+            array.splice(i, 1);
+            console.log(array);
+            console.log('splicing..');
+            array.splice(i, 1);
+            console.log(array);
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkuserid(userId) {
+    console.log('userId:');
+    console.log(userId.userId);
+    console.log('is ' + userId.userId + ' the same as 5fad28a978609200046f23ca ?');
+    return userId.userId === "5fad28a978609200046f23ca";
+}
